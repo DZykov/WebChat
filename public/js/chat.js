1d1d036e66df8fc15250e87127a203b90d36083b
DZykov <dzykov@protonmail.com>
2022-09-15 15:03:38 -0400

    add chat js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,2 +1,10 @@
 const socket = io('localhost:3000');
-console.log(1)
+
+let room_id = document.querySelector('#room-id-new');
+let room_pass = document.querySelector('#room-pas-new');
+let leave_button = document.querySelector('#leave-button');
+let join_button = document.querySelector('#join-button');
+let select_win = document.querySelector('#rooms');
+let users_lsy = document.querySelector('#users');
+let current_room = document.querySelector('#room-id');
+// send button + form
